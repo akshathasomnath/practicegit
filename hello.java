311cd4ab4c759049f142537d645e0e10114ec25f
akshathasomnath <akshathasomnath@gmail.com>
2021-07-13 13:26:07 +0000

    commiting from child

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class hello
 {
 public static void main(String args[])
 {
-System.out.println("Hello, this is the new file");
+System.out.println("Hello, this is the new file, commiting from child");
 }
 }
 
